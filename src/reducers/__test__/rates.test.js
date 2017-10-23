ff6c713575f2f39ea55998c20d39e574ac0653e9
amaj <aaarys@gmail.com>
2017-10-23 10:51:06 +0200

    Add missing test for reducer

---
+++ src/reducers/__test__/rates.test.js
@@ -0,0 +1,45 @@
+import test from 'ava'
+import { OrderedMap, Record } from 'immutable'
+
+import rc from './../../constants/rates-constants'
+import RatesRecord from './../../models/rates-model'
+
+import rates from './../rates'
+
+const initialState = RatesRecord({
+  values: OrderedMap({
+    'USD': 1.0,
+    'EUR': 4.0
+  })
+})
+
+test('set from currency', t => {
+  const state = rates(
+    initialState, {type: rc.SET_FROM_CURRENCY, payload: {currency: 'GBP'}}
+  )
+  t.is(state.from, 'GBP')
+})
+
+test('set to currency', t => {
+  const state = rates(
+    initialState, {type: rc.SET_TO_CURRENCY, payload: {currency: 'EUR'}}
+  )
+  t.is(state.to, 'EUR')
+  t.is(state.from, 'USD')
+})
+
+test('set from value', t => {
+  const state = rates(
+    initialState, {type: rc.SET_FROM_VALUE, payload: {value: 100}}
+  )
+  t.is(state.fromValue, 100)
+  t.is(state.toValue, '400.00')
+})
+
+test('set to value', t => {
+  const state = rates(
+    initialState, {type: rc.SET_TO_VALUE, payload: {value: 200}}
+  )
+  t.is(state.fromValue, '50.00')
+  t.is(state.toValue, 200)
+})
