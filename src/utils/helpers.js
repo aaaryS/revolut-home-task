3671985133088a528d1b1fb1c78dcc89cb80b40b
amaj <aaarys@gmail.com>
2017-10-25 10:32:01 +0200

    Fix linting in utils/helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,6 @@
 import c from './../constants/rates-constants'
 
-export const filterCurrencyValue = v => v.replace(/[\+\-a-zA-Z]/, '')
+export const filterCurrencyValue = v => v.replace(/[+\-a-zA-Z]/, '')
 
 export const sum = (a, b) => (pF(a) + pF(b)).toFixed(c.CURRENCY_PRECISION)
 
